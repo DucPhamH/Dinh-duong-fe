e5cc3f7189256f33f5f6a19530fc98d77934efdc
DucPhamH <ducphamhong2@gmail.com>
2023-03-23 17:31:08 +0700

    fixx hover

--- src/page/Home/CardItem2.jsx
+++ src/page/Home/CardItem2.jsx
@@ -3,7 +3,7 @@ import fruit from '../../asset/img/fruit.jpg'
 
 export default function CardItem2() {
   return (
-    <div className='group my-10 flex w-full h-[36rem] flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md'>
+    <div className='group my-10 flex w-full h-[36rem] hover:border hover:border-red-600 flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md'>
       <Link className='relative mx-3 mt-3 flex h-[60%] overflow-hidden rounded-xl'>
         <img className='absolute top-0 right-0 h-full w-full object-cover' src={fruit} alt='fruit' />
       </Link>
